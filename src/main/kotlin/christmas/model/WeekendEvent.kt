5c32336e5e12c8b5dabcfba9e6bc1952e3521cc3
gaeun <geun5744@gmail.com>
2023-11-14 00:30:24 +0900

    [ADD(WeekendEvent)] 주말 이벤트 필드 추가

---
+++ src/main/kotlin/christmas/model/WeekendEvent.kt
@@ -0,0 +1,11 @@
+package christmas.model
+
+import christmas.model.CalculateResult.getTotalOrderAmount
+import christmas.util.Menu
+
+class WeekendEvent(private val date: Int, private val order: Map<String, Int>) {
+
+    private val period = listOf(1, 2, 8, 9, 15, 16, 22, 23, 29, 30)
+    private val benefitAmount = 2_023
+
+}
