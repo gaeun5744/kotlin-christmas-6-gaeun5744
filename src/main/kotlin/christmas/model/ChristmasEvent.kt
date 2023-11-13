3dc63b31a3f2b09b7cbb3c30673c1b5511d6a7cd
gaeun <geun5744@gmail.com>
2023-11-14 00:42:38 +0900

    [FEAT(ChristmasEvent)] 크리스마스 이벤트 혜택 금액 반환 메서드 구현

--- src/main/kotlin/christmas/model/ChristmasEvent.kt
+++ src/main/kotlin/christmas/model/ChristmasEvent.kt
@@ -7,6 +7,11 @@ class ChristmasEvent(private val date: Int, private val order: Map<String, Int>)
     private val period = (1..25).toList()
     private val discountAmount = 1_000
 
+    fun getDiscountAmount(): Int {
+        if (!checkMatch()) return 0
+        return -(discountAmount + (date - 1) * 100)
+    }
+
     fun checkMatch(): Boolean =
         date in period && getTotalOrderAmount(order) > 10_000
 
