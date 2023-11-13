193317cf7c128d29af2d508e5b5a382de6e0838a
gaeun <geun5744@gmail.com>
2023-11-14 00:31:02 +0900

    [FEAT(WeekendEvent)] 주문 중 메인 메뉴 개수 반환 로직 구현

--- src/main/kotlin/christmas/model/WeekendEvent.kt
+++ src/main/kotlin/christmas/model/WeekendEvent.kt
@@ -8,4 +8,8 @@ class WeekendEvent(private val date: Int, private val order: Map<String, Int>) {
     private val period = listOf(1, 2, 8, 9, 15, 16, 22, 23, 29, 30)
     private val benefitAmount = 2_023
 
+    private fun getCountMain(): Int {
+        val dessertName = Menu.MAIN.detailMenu.map { it.menuName }
+        return dessertName.intersect(order.keys).size
+    }
 }
