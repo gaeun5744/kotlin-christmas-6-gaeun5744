615758f3615aa2e20b93e0e4eacc444cc82c486e
gaeun <geun5744@gmail.com>
2023-11-14 00:33:43 +0900

    [FEAT(SpecialEvent)] 특별 이벤트 적용 여부 확인 메서드 구현

--- src/main/kotlin/christmas/model/SpecialEvent.kt
+++ src/main/kotlin/christmas/model/SpecialEvent.kt
@@ -8,4 +8,8 @@ class SpecialEvent(private val date: Int, private val order: Map<String, Int>) {
     private val period = listOf(3, 10, 17, 24, 45, 31)
     private val benefitAmount = 1_000
 
+
+    fun checkMatch(): Boolean =
+        date in period && getTotalOrderAmount(order) > 10_000
+
 }
