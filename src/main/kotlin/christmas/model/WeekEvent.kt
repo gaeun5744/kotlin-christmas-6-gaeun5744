34cfd6a39aae830e4db5bdeed197b4d9301ce429
gaeun <geun5744@gmail.com>
2023-11-14 00:27:50 +0900

    [FEAT(WeekEvent)] 주문 중 디저트 개수 반환 로직 구현

--- src/main/kotlin/christmas/model/WeekEvent.kt
+++ src/main/kotlin/christmas/model/WeekEvent.kt
@@ -8,4 +8,10 @@ class WeekEvent(private val date: Int, private val order: Map<String, Int>) {
     private val period = listOf(3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 45, 26, 27, 28, 31)
     private val benefitAmount = 2_023
 
+    private fun getCountDessert(): Int {
+        val dessertName = Menu.DESSERT.detailMenu.map { it.menuName }
+        return dessertName.intersect(order.keys).size
+    }
+
+
 }
