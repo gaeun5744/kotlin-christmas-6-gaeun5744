8dee28ec1761edbf61c898577013599d0390cdb1
gaeun <geun5744@gmail.com>
2023-11-14 00:31:48 +0900

    [FEAT(WeekendEvent)] 주말 이벤트 적용 여부 체크 메서드 구현

--- src/main/kotlin/christmas/model/WeekendEvent.kt
+++ src/main/kotlin/christmas/model/WeekendEvent.kt
@@ -8,6 +8,10 @@ class WeekendEvent(private val date: Int, private val order: Map<String, Int>) {
     private val period = listOf(1, 2, 8, 9, 15, 16, 22, 23, 29, 30)
     private val benefitAmount = 2_023
 
+    private fun checkMatch(): Boolean =
+        date in period && getTotalOrderAmount(order) > 0 && getTotalOrderAmount(order) > 10_000
+
+
     private fun getCountMain(): Int {
         val dessertName = Menu.MAIN.detailMenu.map { it.menuName }
         return dessertName.intersect(order.keys).size
