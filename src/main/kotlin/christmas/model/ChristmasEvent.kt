06aec1de747e5e700f8d94f86e6e2f9624ee52a9
gaeun <geun5744@gmail.com>
2023-11-14 00:42:17 +0900

    [FEAT(ChristmasEvent)] 크리스마스 이벤트 적용 여부 체크 메서드 구현

--- src/main/kotlin/christmas/model/ChristmasEvent.kt
+++ src/main/kotlin/christmas/model/ChristmasEvent.kt
@@ -7,4 +7,8 @@ class ChristmasEvent(private val date: Int, private val order: Map<String, Int>)
     private val period = (1..25).toList()
     private val discountAmount = 1_000
 
+    fun checkMatch(): Boolean =
+        date in period && getTotalOrderAmount(order) > 10_000
+
+
 }
