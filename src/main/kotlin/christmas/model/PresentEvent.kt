60702f1f3fe8c772773ab72feb106f31c1d0b91f
gaeun <geun5744@gmail.com>
2023-11-14 00:40:11 +0900

    [FEAT(PresentEvent)] 증정 이벤트 혜택 금액 반환 메서드 구현

--- src/main/kotlin/christmas/model/PresentEvent.kt
+++ src/main/kotlin/christmas/model/PresentEvent.kt
@@ -7,6 +7,11 @@ class PresentEvent(private val date: Int, private val order: Map<String, Int>) {
     private val period = (1..31).toList()
     private val benefitAmount = 25_000
 
+    fun getBenefitAmount(): Int {
+        if (!checkMatch()) return 0
+        return benefitAmount
+    }
+
     fun checkMatch(): Boolean =
         date in period  && getTotalOrderAmount(order) > 12_000
 }
