c261c6634191b89f3a8bdee52482abf418b0cc30
gaeun <geun5744@gmail.com>
2023-11-14 00:32:13 +0900

    [FEAT(WeekendEvent)] 주말 이벤트 적용 혜택 금액 반환 메서드 구현

--- src/main/kotlin/christmas/model/WeekendEvent.kt
+++ src/main/kotlin/christmas/model/WeekendEvent.kt
@@ -8,6 +8,12 @@ class WeekendEvent(private val date: Int, private val order: Map<String, Int>) {
     private val period = listOf(1, 2, 8, 9, 15, 16, 22, 23, 29, 30)
     private val benefitAmount = 2_023
 
+    fun getBenefitAmount(): Int {
+
+        if (!checkMatch()) return 0
+        return -getCountMain() * benefitAmount
+    }
+
     private fun checkMatch(): Boolean =
         date in period && getTotalOrderAmount(order) > 0 && getTotalOrderAmount(order) > 10_000
 
