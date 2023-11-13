4cc614edd198cb539cddd0f8eab5dfc7ea11d2bb
gaeun <geun5744@gmail.com>
2023-11-14 00:37:23 +0900

    [FEAT(SpecialEvent)] 특별 이벤트 혜택 금액 반환 메서드 구현

--- src/main/kotlin/christmas/model/SpecialEvent.kt
+++ src/main/kotlin/christmas/model/SpecialEvent.kt
@@ -8,6 +8,10 @@ class SpecialEvent(private val date: Int, private val order: Map<String, Int>) {
     private val period = listOf(3, 10, 17, 24, 45, 31)
     private val benefitAmount = 1_000
 
+    fun getBenefitAmount(): Int {
+        if (!checkMatch()) return 0
+        return -benefitAmount
+    }
 
     fun checkMatch(): Boolean =
         date in period && getTotalOrderAmount(order) > 10_000
