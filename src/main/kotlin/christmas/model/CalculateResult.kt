5205dac7a945eeb35e43e6b4580cf2f926b8e578
gaeun <geun5744@gmail.com>
2023-11-14 00:43:15 +0900

    [FEAT(CalculateResult)] 총 혜택 금액 반환 메서드 구현

--- src/main/kotlin/christmas/model/CalculateResult.kt
+++ src/main/kotlin/christmas/model/CalculateResult.kt
@@ -4,9 +4,20 @@ import christmas.util.DetailMenu
 
 object CalculateResult{
 
+    fun getTotalBenefit(
+        weekEvent: Int,
+        weekendEvent: Int,
+        specialEvent: Int,
+        presentEvent: Int,
+        christmasEvent: Int
+    ): Int {
+        return weekEvent + weekendEvent + specialEvent + presentEvent + christmasEvent
+    }
+
     fun getTotalOrderAmount(order: Map<String, Int>): Int = order.map { (menu, count) ->
         val menu = DetailMenu.values().first { it.menuName == menu }
         menu.price * count
     }.sum()
+
 
 }
