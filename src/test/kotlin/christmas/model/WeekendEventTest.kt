a276dbb57afaba1186d55e72afff2fc19d6501ce
gaeun <geun5744@gmail.com>
2023-11-14 16:16:16 +0900

    [TEST(WeekendEventTest)] 주말 이벤트 혜택 내역 테스트 구현

--- src/test/kotlin/christmas/model/WeekendEventTest.kt
+++ src/test/kotlin/christmas/model/WeekendEventTest.kt
@@ -13,6 +13,12 @@ class WeekendEventTest {
         assertEquals(weekendEvent.checkMatch(), expectedResult)
     }
 
+    @ParameterizedTest
+    @MethodSource("provideGetBenefitExample")
+    fun `주말 이벡트 혜택 내역 테스트`(weekendEvent: WeekendEvent, expectedResult: Int) {
+        assertEquals(weekendEvent.getBenefitAmount(), expectedResult)
+    }
+
     companion object {
         @JvmStatic
         fun provideCheckMatchExample() = listOf(
@@ -21,5 +27,12 @@ class WeekendEventTest {
             Arguments.of(WeekendEvent(10, mapOf("티본스테이크" to 1, "아이스크림" to 2)), false), // 주문 날짜가 이벤트에 해당하지 않는 경우
         )
 
+        @JvmStatic
+        fun provideGetBenefitExample() = listOf(
+            Arguments.of(WeekendEvent(8, mapOf("양송이수프" to 1, "티본스테이크" to 1, "제로콜라" to 1)), -2_023),
+            Arguments.of(WeekendEvent(8, mapOf("크리스마스파스타" to 3, "레드와인" to 1)), -2_023 * 3),
+            Arguments.of(WeekendEvent(9, mapOf("타파스" to 2, "샴페인" to 1)), 0), // 주문에 메인 메뉴가 없는 경우
+            Arguments.of(WeekendEvent(10, mapOf("해산물파스타" to 1, "아이스크림" to 2)), 0), // 주문 날짜가 이벤트에 해당하지 않는 경우
+        )
     }
 }
