1215ac1cf75f89b1a2f0ce6180fb96beab9d29fa
gaeun <geun5744@gmail.com>
2023-11-15 00:10:28 +0900

    [FEAT(OutputView)] 주문 메뉴 출력 메서드 구현

--- src/main/kotlin/christmas/view/OutputView.kt
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -1,9 +1,14 @@
 package christmas.view
 
+import christmas.util.Badge
+import java.text.DecimalFormat
+
 class OutputView {
-    fun printMenu() {
+    fun printMenu(order: Map<String, Int>) {
         println("<주문 메뉴>")
-        // ...
+        order.forEach { (menu, count) ->
+            println("$menu ${count}개")
+        }
     }
-    // ...
+
 }
