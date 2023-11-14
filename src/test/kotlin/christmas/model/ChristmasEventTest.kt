e234083e0e7e793ee8bd261e358f22bb8ff734bc
gaeun <geun5744@gmail.com>
2023-11-14 16:55:14 +0900

    [Test(ChristmasEventTest)] 크리스마스 이벤트 혜택 내역 반환 테스트 구현

--- src/test/kotlin/christmas/model/ChristmasEventTest.kt
+++ src/test/kotlin/christmas/model/ChristmasEventTest.kt
@@ -12,6 +12,12 @@ class ChristmasEventTest {
         assertEquals(christmasEvent.checkMatch(), expectedResult)
     }
 
+    @ParameterizedTest
+    @MethodSource("provideGetBenefitExample")
+    fun `크리스마스 이벡트 혜택 내역 테스트`(christmasEvent: ChristmasEvent, expectedResult: Int) {
+        assertEquals(christmasEvent.getDiscountAmount(), expectedResult)
+    }
+
     companion object {
         @JvmStatic
         fun provideCheckMatchExample() = listOf(
@@ -23,5 +29,15 @@ class ChristmasEventTest {
             Arguments.of(ChristmasEvent(10, mapOf("시저샐러드" to 1)), false) // 총 주문 금액이 10000원 미만인 경우
         )
 
+        @JvmStatic
+        fun provideGetBenefitExample() = listOf(
+            Arguments.of(ChristmasEvent(4, mapOf("양송이수프" to 1, "초코케이크" to 1, "레드와인" to 1)), -1_300),
+            Arguments.of(ChristmasEvent(25, mapOf("초코케이크" to 3, "레드와인" to 1, "해산물파스타" to 2)), -3_400),
+            Arguments.of(
+                ChristmasEvent(28, mapOf("해산물파스타" to 1, "아이스크림" to 2, "타파스" to 1)),
+                0
+            ), // 주문 날짜가 이벤트에 해당하지 않는 경우
+            Arguments.of(ChristmasEvent(11, mapOf("아이스크림" to 1, "제로콜라" to 1)), 0) // 총 주문 금액이 10000원 미만인 경우
+        )
     }
 }
