3fe94eef58e0c385063d28fc159b8fbbefe9a98f
gaeun <geun5744@gmail.com>
2023-11-14 21:35:20 +0900

    [TEST(ValidationTest)] 유효하지 않은 주문 및 에러메시지 테스트 구현

--- src/test/kotlin/christmas/util/ValidationTest.kt
+++ src/test/kotlin/christmas/util/ValidationTest.kt
@@ -14,4 +14,13 @@ class ValidationTest {
         }.isInstanceOf(IllegalArgumentException::class.java).hasMessageContaining("[ERROR]")
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = ["아스파라거스-1,해산물파스타-1", "타파스-0,티본스테이크-2", "시저샐러드 3개", "제로콜라-1/바비큐립-1"])
+    fun `유효하지 않은 주문 테스트`(order: String) {
+        assertThatThrownBy {
+            Validation.checkMenu(order)
+        }.hasMessageContaining("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+    }
+
+
 }
