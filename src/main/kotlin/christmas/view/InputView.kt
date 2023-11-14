c50cf494702d1ae922d484b397708cf32c38e512
gaeun <geun5744@gmail.com>
2023-11-14 23:21:47 +0900

    [FEAT(InputView)] 유효한 날짜를 입력 받을 때까지 반복하는 로직 구현

--- src/main/kotlin/christmas/view/InputView.kt
+++ src/main/kotlin/christmas/view/InputView.kt
@@ -1,13 +1,26 @@
 package christmas.view
 
 import camp.nextstep.edu.missionutils.Console
+import christmas.util.Validation
 
 class InputView {
     fun readDate(): Int {
         println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
-        val input = Console.readLine()
-        // ...
-        return 0
+        return getValidDate()
     }
-    // ...
+
+    private fun getValidDate(): Int {
+        while (true) {
+            try {
+                val input = Console.readLine()
+                Validation.checkDate(input)
+                return input.toInt()
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            } catch (e: IndexOutOfBoundsException) {
+                println(e.message)
+            }
+        }
+    }
+
 }
