1b3c5d92dcbdee3b462438282d9f52bd22fc2120
gaeun <geun5744@gmail.com>
2023-11-14 20:52:40 +0900

    [FEAT(Validation)] 메뉴 주문 형식 유효성 검사 메서드 구현

--- src/main/kotlin/christmas/util/Validation.kt
+++ src/main/kotlin/christmas/util/Validation.kt
@@ -1,26 +1,45 @@
 package christmas.util
 
 import christmas.util.ErrorMessage.INVALID_DATE_RANGE
+import christmas.util.ErrorMessage.INVALID_MENU
 import christmas.util.ErrorMessage.NOT_NUM
 
 object Validation {
 
-    fun checkDate(date:String){
+    fun checkDate(date: String) {
         checkIsDigit(date)
         checkDateRange(date)
     }
 
-    private fun checkIsDigit(date: String){
+    private fun checkIsDigit(date: String) {
         try {
             date.toInt()
-        }catch (e:NumberFormatException){
+        } catch (e: NumberFormatException) {
             throw NumberFormatException(NOT_NUM)
         }
     }
 
-    private fun checkDateRange(date: String){
-        require(date.toInt() in 1..31){
+    private fun checkDateRange(date: String) {
+        require(date.toInt() in 1..31) {
             INVALID_DATE_RANGE
         }
     }
+
+    fun checkMenu(order: String) {
+        checkValidForm(order)
+    }
+
+    private fun checkValidForm(order: String) {
+        try {
+            getRefinedOrder(order)
+        } catch (e: IllegalArgumentException) {
+            throw IllegalArgumentException(INVALID_MENU)
+        }
+    }
+
+    private fun getRefinedOrder(order: String) = order.split(",").associate { order ->
+        val (key, value) = order.split("-")
+        key to value.toInt()
+    }
+
 }
