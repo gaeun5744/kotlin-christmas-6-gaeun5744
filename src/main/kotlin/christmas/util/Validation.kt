b584ce4a37a5288f27e9911ce4c7fd8c521b3f85
gaeun <geun5744@gmail.com>
2023-11-14 18:23:41 +0900

    [FEAT(Validation)] 날짜 입력 예외 전환 메서드 구현

--- src/main/kotlin/christmas/util/Validation.kt
+++ src/main/kotlin/christmas/util/Validation.kt
@@ -1,4 +1,26 @@
 package christmas.util
 
+import christmas.util.ErrorMessage.INVALID_DATE_RANGE
+import christmas.util.ErrorMessage.NOT_NUM
+
 object Validation {
+
+    fun checkDate(date:String){
+        checkIsDigit(date)
+        checkDateRange(date)
+    }
+
+    private fun checkIsDigit(date: String){
+        try {
+            date.toInt()
+        }catch (e:NumberFormatException){
+            throw NumberFormatException(NOT_NUM)
+        }
+    }
+
+    private fun checkDateRange(date: String){
+        require(date.toInt() in 1..31){
+            INVALID_DATE_RANGE
+        }
+    }
 }
