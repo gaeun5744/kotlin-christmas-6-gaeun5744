08c2ca68885b65d14a3bc1c30c63c1ba50a8c526
gaeun <geun5744@gmail.com>
2023-11-14 16:01:08 +0900

    [FEAT(WeekEventTest)] 평일 이벤트 혜택 내역 반환 테스트 구현

--- src/test/kotlin/christmas/model/WeekEventTest.kt
+++ src/test/kotlin/christmas/model/WeekEventTest.kt
@@ -1,7 +1,7 @@
 package christmas.model
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -14,6 +14,12 @@ class WeekEventTest {
         assertThat(weekEvent.checkMatch()).isEqualTo(expectedResult)
     }
 
+    @ParameterizedTest
+    @MethodSource("provideGetBenefitExample")
+    fun `평일 이벡트 혜택 내역 테스트`(weekEvent: WeekEvent, expectedResult: Int) {
+        assertEquals(weekEvent.getBenefitAmount(), expectedResult)
+    }
+
     companion object {
         @JvmStatic
         fun provideCheckMatchExample() = listOf(
@@ -21,6 +27,15 @@ class WeekEventTest {
             Arguments.of(WeekEvent(14, mapOf("바비큐립" to 3, "제로콜라" to 1)), false), // 주문에 디저트가 없는 경우
             Arguments.of(WeekEvent(8, mapOf("티본스테이크" to 1, "아이스크림" to 2)), false), // 주문 날짜가 이벤트에 해당하지 않는 경우
             Arguments.of(WeekEvent(11, mapOf("아이스크림" to 1)), false) // 총 주문 금액이 10000원 미만인 경우
+        )
+
+        @JvmStatic
+        fun provideGetBenefitExample() = listOf(
+            Arguments.of(WeekEvent(4, mapOf("양송이수프" to 1, "초코케이크" to 1, "레드와인" to 1)), -2_023),
+            Arguments.of(WeekEvent(12, mapOf("초코케이크" to 3, "레드와인" to 1)), -2_023 * 3),
+            Arguments.of(WeekEvent(26, mapOf("크리스마스파스타" to 3, "샴페인" to 1)), 0), // 주문에 디저트가 없는 경우
+            Arguments.of(WeekEvent(23, mapOf("해산물파스타" to 1, "아이스크림" to 2)), 0), // 주문 날짜가 이벤트에 해당하지 않는 경우
+            Arguments.of(WeekEvent(11, mapOf("아이스크림" to 1)), 0) // 총 주문 금액이 10000원 미만인 경우
         )
     }
 
