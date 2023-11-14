307dbb4c526515460fc2a4fe9754c0c8a1cd22b4
gaeun <geun5744@gmail.com>
2023-11-14 23:24:37 +0900

    [FEAT(InputView)] 유효한 주문을 입력 받을 때까지 반복하는 로직 구현

--- src/main/kotlin/christmas/view/InputView.kt
+++ src/main/kotlin/christmas/view/InputView.kt
@@ -9,6 +9,11 @@ class InputView {
         return getValidDate()
     }
 
+    fun readOrder(): Map<String,Int> {
+        println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
+        return getValidOrder()
+    }
+
     private fun getValidDate(): Int {
         while (true) {
             try {
@@ -21,6 +26,25 @@ class InputView {
                 println(e.message)
             }
         }
+    }
+
+    private fun getValidOrder():Map<String,Int>{
+        while (true) {
+            try {
+                val input = Console.readLine()
+                Validation.checkMenu(input)
+                return getRefinedOrder(input)
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            } catch (e: IndexOutOfBoundsException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun getRefinedOrder(order: String) = order.split(",").associate { order ->
+        val (key, value) = order.split("-")
+        key to value.toInt()
     }
 
 }
