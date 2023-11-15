d494bfb2d127451a7c8a6706b1255511cbbca53d
gaeun <geun5744@gmail.com>
2023-11-15 17:08:20 +0900

    [TEST(OrderManagerTest)] String -> Map 변환 테스트

--- src/test/kotlin/christmas/util/OrderManagerTest.kt
+++ src/test/kotlin/christmas/util/OrderManagerTest.kt
@@ -10,4 +10,11 @@ class OrderManagerTest {
         val order = mapOf("시저샐러드" to 1, "티본스테이크" to 1, "아이스크림" to 1)
         assertEquals(OrderManager.getTotalOrderAmount(order), 68_000)
     }
+
+    @Test
+    fun `주문 String 데이터 Map 으로 변환 테스트`() {
+        val order = "양송이수프-1,해산물파스타-1,샴페인-2"
+        assertEquals(OrderManager.getRefinedOrder(order), mapOf("양송이수프" to 1, "해산물파스타" to 1, "샴페인" to 2))
+    }
+
 }
