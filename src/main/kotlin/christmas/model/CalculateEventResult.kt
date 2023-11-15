6c1f73f2bb2c85d20e00f62711fa33c2d75f1c36
gaeun <geun5744@gmail.com>
2023-11-15 15:04:16 +0900

    [ADD(CalculateEventResult)] 혜택 내역에 증정 이벤트 추가

--- src/main/kotlin/christmas/model/CalculateEventResult.kt
+++ src/main/kotlin/christmas/model/CalculateEventResult.kt
@@ -30,6 +30,7 @@ class CalculateEventResult(
         if (weekEvent.checkMatch()) benefitDetails["평일 할인"] = weekEvent.getBenefitAmount()
         if (weekendEvent.checkMatch()) benefitDetails["주말 할인"] = weekendEvent.getBenefitAmount()
         if (specialEvent.checkMatch()) benefitDetails["특별 할인"] = specialEvent.getBenefitAmount()
+        if (presentEvent.checkMatch()) benefitDetails["증정 이벤트"] = presentEvent.getBenefitAmount()
         if (christmasEvent.checkMatch()) benefitDetails["크리스마스 디데이 할인"] = christmasEvent.getDiscountAmount()
         return benefitDetails
     }
