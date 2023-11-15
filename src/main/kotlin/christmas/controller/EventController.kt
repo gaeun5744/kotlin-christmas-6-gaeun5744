2df2cd02e3eefed6ec2caa34ad628879560ada68
gaeun <geun5744@gmail.com>
2023-11-15 15:12:44 +0900

    [REFACTOR(EventController)] run 메서드 내 로직 더 작게 세분화

--- src/main/kotlin/christmas/controller/EventController.kt
+++ src/main/kotlin/christmas/controller/EventController.kt
@@ -5,16 +5,13 @@ import christmas.util.OrderManager.getTotalOrderAmount
 import christmas.view.InputView
 import christmas.view.OutputView
 
-class EventController {
-
-    private val inputView = InputView()
-    private val outputView = OutputView()
+class EventController(
+    private val inputView: InputView = InputView(),
+    private val outputView: OutputView = OutputView()
+) {
 
     fun run() {
-        outputView.printIntroduce()
-        val date = inputView.readDate()
-        val order = inputView.readOrder()
-        outputView.printEventGuide(date).also { println() }
+        val (date, order) = getInput()
 
         val weekEvent = WeekEvent(date, order)
         val weekendEvent = WeekendEvent(date, order)
@@ -25,6 +22,14 @@ class EventController {
         val calculateEventResult =
             CalculateEventResult(weekEvent, weekendEvent, specialEvent, presentEvent, christmasEvent)
 
+        printResult(order, presentEvent, calculateEventResult)
+    }
+
+    private fun printResult(
+        order: Map<String, Int>,
+        presentEvent: PresentEvent,
+        calculateEventResult: CalculateEventResult
+    ) {
         outputView.printMenu(order).also { println() }
         outputView.printTotalOrderAmount(getTotalOrderAmount(order)).also { println() }
         outputView.printPresent(presentEvent.checkMatch()).also { println() }
@@ -33,6 +38,14 @@ class EventController {
         outputView.printActualPayment(getTotalOrderAmount(order) + calculateEventResult.getTotalBenefit())
             .also { println() }
         outputView.printBadge(calculateEventResult.getEventBadge())
+    }
+
+    private fun getInput(): Pair<Int, Map<String, Int>> {
+        outputView.printIntroduce()
+        val date = inputView.readDate()
+        val order = inputView.readOrder()
+        outputView.printEventGuide(date).also { println() }
+        return Pair(date, order)
     }
 
 }
